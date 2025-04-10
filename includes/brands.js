85efe6ae3955155d42cf69fe2ed9a1a3f4595349
Andre Arias <andre.arias@directwinesinc.com>
2025-04-10 21:45:59 +0000

    test ref

---
+++ includes/brands.js
@@ -0,0 +1,51 @@
+module.exports = [
+  {
+    brand: "LAW",
+    property_id: "287163560",
+    dataset: "superform_outputs_287163560",
+    hostname: ["www.laithwaites.com", "auth.laithwaites.com"],
+    // Unique name for the Dataform declaration
+    sourceDeclarationName: "ga4_sessions_law"
+  },
+  {
+    brand: "WSJ",
+    property_id: "287832387",
+    dataset: "superform_outputs_287832387",
+    hostname: ["www.wsjwine.com", "auth.wsjwine.com"],
+    // Unique name for the Dataform declaration
+    sourceDeclarationName: "ga4_sessions_wsj"
+  },
+  {
+    brand: "TCM",
+    property_id: "358775017",
+    dataset: "superform_outputs_358775017",
+    hostname: ["shop.tcmwineclub.com", "auth.shop.tcmwineclub.com"],
+    // Unique name for the Dataform declaration
+    sourceDeclarationName: "ga4_sessions_tcm"
+  },
+  {
+    brand: "NPR",
+    property_id: "373836794",
+    dataset: "superform_outputs_373836794",
+    hostname: ["www.nprwineclub.org", "auth.nprwineclub.org"],
+    // Unique name for the Dataform declaration
+    sourceDeclarationName: "ga4_sessions_npr"
+  },
+  {
+    brand: "NGO",
+    property_id: "391406454",
+    dataset: "superform_outputs_391406454",
+    hostname: ["www.natgeowine.com", "auth.natgeowine.com"],
+    // Unique name for the Dataform declaration
+    sourceDeclarationName: "ga4_sessions_ngo"
+  },
+  {
+    brand: "OSW",
+    property_id: "425709539",
+    dataset: "superform_outputs_425709539",
+    hostname: ["www.omahasteakswine.com", "auth.omahasteakswine.com"],
+    // Unique name for the Dataform declaration
+    sourceDeclarationName: "ga4_sessions_osw"
+  }
+  // Add any other brands similarly
+];
