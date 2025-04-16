811dea5fd7413e56cf751f30ac6526ad5934f309
Andre Arias <andre.arias@directwinesinc.com>
2025-04-16 03:51:37 +0000

    update rec008 to staging

--- includes/brands.js
+++ includes/brands.js
@@ -1,47 +1,38 @@
-module.exports = {
-  brands: [
-    {
-      brand: "LAW",
-      property_id: "287163560",
-      dataset: "superform_outputs_287163560",
-      hostname: ["www.laithwaites.com", "auth.laithwaites.com"],
-      sourceDeclarationName: "ga4_sessions_law" 
-    },
-    {
-      brand: "WSJ",
-      property_id: "287832387",
-      dataset: "superform_outputs_287832387",
-      hostname: ["www.wsjwine.com", "auth.wsjwine.com"],
-      sourceDeclarationName: "ga4_sessions_wsj"
-    },
-    {
-      brand: "TCM",
-      property_id: "358775017",
-      dataset: "superform_outputs_358775017",
-      hostname: ["shop.tcmwineclub.com", "auth.shop.tcmwineclub.com"],
-      sourceDeclarationName: "ga4_sessions_tcm"
-    },
-    {
-      brand: "NPR",
-      property_id: "373836794",
-      dataset: "superform_outputs_373836794",
-      hostname: ["www.nprwineclub.org", "auth.nprwineclub.org"],
-      sourceDeclarationName: "ga4_sessions_npr"
-    },
-    {
-      brand: "NGO",
-      property_id: "391406454",
-      dataset: "superform_outputs_391406454",
-      hostname: ["www.natgeowine.com", "auth.natgeowine.com"],
-      sourceDeclarationName: "ga4_sessions_ngo"
-    },
-    {
-      brand: "OSW",
-      property_id: "425709539",
-      dataset: "superform_outputs_425709539",
-      hostname: ["www.omahasteakswine.com", "auth.omahasteakswine.com"],
-      sourceDeclarationName: "ga4_sessions_osw"
-    }
-    // Add more brands here
-  ]
-};
+module.exports = [
+  {
+    brand: "LAW",
+    property_id: "287163560",
+    dataset: "superform_outputs_287163560",
+    hostname: ["www.laithwaites.com", "auth.laithwaites.com"]
+  },
+  {
+    brand: "WSJ",
+    property_id: "287832387",
+    dataset: "superform_outputs_287832387",
+    hostname: ["www.wsjwine.com", "auth.wsjwine.com"]
+  },
+  {
+    brand: "TCM",
+    property_id: "358775017",
+    dataset: "superform_outputs_358775017",
+    hostname: ["shop.tcmwineclub.com", "auth.shop.tcmwineclub.com"]
+  },
+  {
+    brand: "NPR",
+    property_id: "373836794",
+    dataset: "superform_outputs_373836794",
+    hostname: ["www.nprwineclub.org", "auth.nprwineclub.org"]
+  },
+  {
+    brand: "NGO",
+    property_id: "391406454",
+    dataset: "superform_outputs_391406454",
+    hostname: ["www.natgeowine.com", "auth.natgeowine.com"]
+  },
+  {
+    brand: "OSW",
+    property_id: "425709539",
+    dataset: "superform_outputs_425709539",
+    hostname: ["www.omahasteakswine.com", "auth.omahasteakswine.com"]
+  }
+];
