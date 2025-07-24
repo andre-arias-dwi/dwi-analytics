ad428d8aa90e8c7305e4195160f1f2d4875b5c9d
Andre Arias <andre.arias.dw@gmail.com>
2025-07-24 17:59:24 +0000

    presale final version

--- ad_hoc_sql/2025/07/2025-07-24_magpie_presale_users_orders_J06392.sql
+++ ad_hoc_sql/2025/07/2025-07-24_magpie_presale_users_orders_J06392.sql
@@ -1,38 +1,25 @@
- with visits as (
-  select
-  brand,
-  event_date,
-  session_id,
-  user_pseudo_id
-  from tough-healer-395417.analytics_unified.fact_ga4_events
-  where regexp_contains(page_location, r'/newlandingpage/|/offer_temp5\.jsp')
-  and regexp_contains(page_location, r'AGXN003|AGXP003')
-  and event_date >= '2025-06-10'
-  group by all
-  ),
-  
-  transactions as(
-    select distinct
-    v.session_id,
-    t.transaction_id,
-    t.ecommerce.purchase_revenue
-    from visits v
-    left join tough-healer-395417.analytics_unified.fact_ga4_transactions t
-    using (session_id)
-  ),
-  
-  filtered_transactions as (
-  select distinct
-  t.*
-  from transactions t
-  left join tough-healer-395417.analytics_reporting.rpt_ga4_transactions_items i
-  using (transaction_id)
-  where i.item_id = '20200SV'
-  )
-  select
+WITH presale_visits AS (
+  SELECT
+    brand,
+    event_date,
+    session_id,
+    user_pseudo_id
+  FROM `analytics_unified.fact_ga4_events`
+  WHERE event_date >= '2025-06-10'
+    AND REGEXP_CONTAINS(page_location, r'/newlandingpage/|/offer_temp5\.jsp')
+    AND REGEXP_CONTAINS(page_location, r'AGXN003|AGXP003')
+  GROUP BY 1,2,3,4
+)
+
+SELECT
   v.*,
-  f.transaction_id,
-  f.purchase_revenue
-  from visits v
-  left join filtered_transactions f
-  using (session_id)
+  t.transaction_id,
+  t.ecommerce.purchase_revenue
+FROM presale_visits v
+LEFT JOIN `analytics_unified.fact_ga4_transactions`           t
+       USING (session_id)
+LEFT JOIN `analytics_reporting.rpt_ga4_transactions_items`   i
+       ON  t.transaction_id = i.transaction_id
+       AND i.item_id       = '20200SV'   -- <<< filter lives in ON clause
+WHERE t.transaction_id IS NULL           -- keep all non-buyers
+   OR i.item_id       = '20200SV';       -- keep only Magpie buyers
